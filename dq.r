f3512f19ec49aa4ef277dd7bd107b8fff4d32528
unknown <Balaji.Subudhi@hmecl000123.happiestminds.com>
2015-05-02 17:38:39 +0530

    1st

--- dq.r
+++ dq.r
@@ -1,9 +1,9 @@
 library(shiny)
 runApp(appDir = "C:\\Users\\balaji.subudhi\\Desktop\\dq",port = 1009)
 
-library(shinyapps)
-shinyapps::setAccountInfo(name="ebsubudhi", token="624801D70E3FE8F4734ED8DF4C53456C", secret="lpaC80XR5aZn9I25mKR/UiA+nES9QMtnWUEfTt8z")
-deployApp(appDir = "C:\\Users\\balaji.subudhi\\Desktop\\dq1", appName = "DataQuality", account = "ebsubudhi")
+
+
+# hello balaji
 
 
 cred <- OAuthFactory$new(consumerKey="de4vV4YMqJG8uc02FnbW8oxMy",
